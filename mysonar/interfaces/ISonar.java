794b242a58e25dc6bd2898bc19f656c24d78d772
Hiari Pizzini Cavagna <h.pizzinicavagna@gmail.com>
2022-03-15 13:10:49 +0100

    Delete mysonar directory

--- mysonar/interfaces/ISonar.java
+++
@@ -1,8 +0,0 @@
-package it.unibo.radarSystem22.domain.interfaces;
-
-public interface ISonar {
-	public void activate();
-	public void deactivate();
-	public IDistance getDistance();
-	public boolean isActive();
-}
